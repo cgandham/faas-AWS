81325e8e1b73709648aded72eb12e6d15fb6c7dc
Chandana Sri Gandham <47238951+cgandham@users.noreply.github.com>
2023-02-28 15:38:13 -0800

    Update EmailTask.java

--- src/main/java/faas/cloud/demo/Events/EmailTask.java
+++ src/main/java/faas/cloud/demo/Events/EmailTask.java
@@ -62,12 +62,7 @@ public class EmailTask { //implements RequestHandler<SNSEvent,Object>
 //            AmazonDynamoDB c = new AmazonDynamoDBClient(awsCreds);
 //            this.dynamoDB = new DynamoDB(c);
             context.getLogger().log("Entered Lambda Function Code");
-//            String emailVal = "";
-//            String token = UUID.randomUUID().toString();
-//            emailVal += "<p><a href='#'>http://" + domain +"/forgotPWD/"+token+ "</a></p><br>";
-//            emailVal =  emailVal.replaceAll("\"","");
-//            context.getLogger().log(emailVal);
-//            sendEMAIL(context,"chandana.sri.g@gmail.com",emailVal);
+
             if(dynamoDB == null)
                 context.getLogger().log("Dynamo db object is null");
             TableCollection<ListTablesResult> dbTables = dynamoDB.listTables();
@@ -96,7 +91,6 @@ public class EmailTask { //implements RequestHandler<SNSEvent,Object>
             Item item = dynamoDB.getTable("csye6225").getItem("id", toEMAIL);
             long ttlTime = Instant.now().getEpochSecond() + 15*60;
             if ((item != null && Long.parseLong(item.get("ttl").toString()) < Instant.now().getEpochSecond() || item == null)) {
-             //   Item itemToAdd = new Item().withString("id", toEMAIL).withLong("ttl", ttlTime);
                 PutItemSpec item2 = new PutItemSpec().withItem(new Item()
                         .withPrimaryKey("id", toEMAIL)
                         .withString("token", token)
@@ -132,7 +126,7 @@ public class EmailTask { //implements RequestHandler<SNSEvent,Object>
 //                    AmazonSimpleEmailServiceClientBuilder.standard()
 //                            .withRegion(Regions.US_EAST_1)
 //                            .build();
-          //  System.out.println("Connected to Amazon SES!");
+
             context.getLogger().log("Connected to Amazon SES!");
 
             SendEmailRequest req = new SendEmailRequest()
@@ -157,65 +151,4 @@ public class EmailTask { //implements RequestHandler<SNSEvent,Object>
 
         }
     }
-
-//    public void getMsgFrmSQS(AWSCredentials awsCreds,Context context){
-//        context.getLogger().log("Getting msgs from SQS");
-//        AmazonSQS sqsClient;
-//        sqsClient = new AmazonSQSClient(awsCreds);
-//        sqsClient.setRegion(Region.getRegion(Regions.US_EAST_1));
-//      //  String queueUrl = sqsClient.getQueueUrl(QUEUE).getQueueUrl();
-//        context.getLogger().log("SQS client created");
-//
-//
-//
-//        List<com.amazonaws.services.sqs.model.Message> receivedMessageList =
-//                sqsClient.receiveMessage(sqsClient.getQueueUrl(QUEUE).getQueueUrl()).getMessages();
-//        for(com.amazonaws.services.sqs.model.Message message : receivedMessageList) {
-//            if (message.getBody() !=null && !message.getBody().isEmpty()) {
-//                context.getLogger().log("Extracting recipient email id to send emails");
-//                JsonObject jsonObject = new JsonParser().parse(message.getBody()).getAsJsonObject();
-//                String toEMAIL = jsonObject.get("email").getAsString();
-//
-//                checkDynamoDBTable( context, toEMAIL, token);
-//                sendEMAIL(context,toEMAIL,emailVal);
-//
-//            }
-//        }
-//    }
-
-//    public void checkDynamoDBTable(Context context,String toEMAIL,String token){
-//        context.getLogger().log("Reached checkDynamoDBTable");
-//        // unix time
-//        long now = Calendar.getInstance().getTimeInMillis()/1000;
-//        //setting TTL to 60 minutes
-//        long TTL = 15 * 60;
-//        long totalTTL = TTL + now ;
-//
-//        Table table = dynamoDB.getTable(dynamodbTable);
-//        if(table == null) {
-//            context.getLogger().log("DynamoDB table not found");
-//        }
-//        else {
-//            context.getLogger().log("checking in table for token for email: "+toEMAIL);
-//            long ttlDBValue = 0;
-//            Item item = table.getItem("email", toEMAIL);
-//
-//            if (item != null)
-//                ttlDBValue = item.getLong("ttl");
-//
-//            if(item == null || (ttlDBValue < now && ttlDBValue != 0)) {
-//                System.out.println("creating new token and sending email to: "+toEMAIL);
-//                context.getLogger().log("ttl expired, creating new token and sending email");
-//                table.putItem( new PutItemSpec().withItem(new Item()
-//                        .withPrimaryKey("email", toEMAIL)
-//                        .withString("token", token)
-//                        .withLong("ttl", totalTTL)));
-//
-//                context.getLogger().log("AWS request ID:" + context.getAwsRequestId());
-//                //context.getLogger().log("AWS message ID:" + input.getRecords().get(0).getSNS().getMessageId());
-//            } else {
-//                context.getLogger().log(item.toJSON() + "Email already sent!");
-//            }
-//        }
-//    }
 }
